e8f8efbfb8e3b4a2b97a150f0a9437f30adb9601
Zyadddd <zyadrachdi@gmail.com>
2025-01-16 14:34:13 -0700

    Update threeScene.js

--- src/threeScene.js
+++ src/threeScene.js
@@ -22,8 +22,6 @@ export function createThreeScene(containerSelector, objPath) {
   renderer.setSize(container.clientWidth, container.clientHeight);
   container.appendChild(renderer.domElement);
 
-  const material = new THREE.Material(MeshDepthMaterial);
-
   // Add lighting
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   directionalLight.position.set(5, 5, 5);
