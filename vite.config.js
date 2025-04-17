7cfa48a810ae589b571ce093dcb973b2c9ae1459
Zyad <zyadrachdi@gmail.com>
2025-04-17 17:53:19 -0600

    i thinkkkkk

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
       input: {
         main: 'index.html',
         a2project: 'src/a2project.html',
-        a3project: 'src/a3project.html' // Ensure this file exists
+        a3project: 'src/a3project.html', // Ensure this file exists
+        a4project: 'src/a4project.html'  // Add a4project to the build
       }
     }
   }
