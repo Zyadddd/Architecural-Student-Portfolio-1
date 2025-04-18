9a66f329e4db328ed6a1e6838a295987a27d3ddf
Zyad <zyadrachdi@gmail.com>
2025-04-17 18:04:37 -0600

    rehaibfgeiw;jbg

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         main: 'index.html',
         a2project: 'src/a2project.html',
         a3project: 'src/a3project.html', // Ensure this file exists
-        a4project: 'src/a4project.html'  // Add a4project to the build
+        a4project: 'src/a4project.html',  // Add a4project to the build
+        a5project: 'src/a5project.html'  // Add a5project
       }
     }
   }
